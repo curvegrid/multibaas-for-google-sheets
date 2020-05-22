cb4e580f1476fe2a85198caf13add384797e0113
Daenam Kim <daenam.kim@gmail.com>
2020-05-23 08:41:32 +0900

    Fix eslint fails

--- test/auth.js
+++ test/auth.js
@@ -51,6 +51,7 @@ function getAccessToken(scopes, oAuth2Client, callback) {
         console.log(`token stored to ${TOKEN_PATH}`);
       });
       callback(oAuth2Client);
+      return undefined;
     });
   });
 }
@@ -62,6 +63,7 @@ function getAccessToken(scopes, oAuth2Client, callback) {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(scopes, credentials, callback) {
+  // eslint-disable-next-line camelcase
   const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
@@ -73,22 +75,21 @@ function authorize(scopes, credentials, callback) {
 
     oAuth2Client.setCredentials(JSON.parse(token));
     callback(oAuth2Client);
+    return undefined;
   });
 }
 
 // Load and return client secrets from a local file
 // @param scopes [Array] A list of auth scopes (optional)
 // @return {google.auth.OAuth2} An authorized OAuth2 client
-module.exports.authenticate = (scopes = SCOPES) => {
-  return new Promise((resolve, reject) => {
-    // Load client secrets from a local file.
-    fs.readFile(CLIENT_SECRET_PATH, (err, credentials) => {
-      if (err) {
-        reject(err);
-      }
+module.exports.authenticate = (scopes = SCOPES) => new Promise((resolve, reject) => {
+  // Load client secrets from a local file.
+  fs.readFile(CLIENT_SECRET_PATH, (err, credentials) => {
+    if (err) {
+      reject(err);
+    }
 
-      // Authorize a client with credentials, then call the Google Drive API.
-      authorize(scopes, JSON.parse(credentials), resolve);
-    });
+    // Authorize a client with credentials, then call the Google Drive API.
+    authorize(scopes, JSON.parse(credentials), resolve);
   });
-};
+});
